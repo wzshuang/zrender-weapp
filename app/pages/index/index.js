4ed00cc10b2b7df67c56d5f189d93867c114bf78
unknown <443509543@qq.com>
2016-12-27 23:56:37 +0800

    update

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,53 +1,84 @@
 var zrender = require('./zrender/zrender')
 var CircleShape = require('./zrender/graphic/shape/Circle')
+var PolylineShape = require('./zrender/graphic/shape/Polyline')
+var PolygonShape = require('./zrender/graphic/shape/Polygon')
 var ZText = require('./zrender/graphic/Text')
-var BezierCurve = require('./zrender/graphic/shape/BezierCurve')
+var StarShape = require('./zrender/graphic/shape/Star')
 var WeCanvas = require('./zrender/WeCanvas')
 Page({
   canvasIdErrorCallback: function (e) {
     console.error(e.detail.errMsg)
   },
   onReady: function (e) {
-    var circle = new CircleShape({
-      shape: {
-        cx: 100,
-        cy: 100,
-        r: 50
-      },
+    var star = new StarShape({
       style: {
-        fill: '#ff0000'
+        lineDash: [5, 5],
+        stroke: 'black'
+      },
+      shape: {
+        n: 5,
+        r: 50,
+        cx: 50,
+        cy: 80
       }
     });
+
+    var ctx1 = new WeCanvas("canvas-1");
+    var zr1 = zrender.init(ctx1);
+    zr1.add(star);
+    star.animate('style', true)
+      .when(1000, {
+        lineDashOffset: -5
+      })
+      .start();
+
+
     var text = new ZText({
       style: {
         x: 10,
         y: 50,
         text: 'zrender in\nwechat\n小程序',
         fill: '#ff0000',
-        textFont: '18px Microsoft Yahei',
+        textFont: '20px Microsoft Yahei',
         textBaseline: 'top'
       },
       draggable: true
     });
-    var curve = new BezierCurve({
-      style: {
-        lineDash: [5, 5],
-        lineDashOffset: 0
-      },
+    var ctx2 = new WeCanvas("canvas-2");
+    var zr2 = zrender.init(ctx2);
+    zr2.add(text);
+
+    var circle = new CircleShape({
       shape: {
-        x1: 100, y1: 100,
-        x2: 100, y2: 500,
-        x3: 500, y3: 100,
-        x4: 500, y4: 500
+        cx: 60,
+        cy: 80,
+        r: 50
+      },
+      style: {
+        fill: '#ff0000'
       }
     });
+    var ctx3 = new WeCanvas("canvas-3");
+    var zr3 = zrender.init(ctx3);
+    zr3.add(circle);
 
-    var firstCanvas = new WeCanvas("firstCanvas");
-    var zr1 = zrender.init(firstCanvas);
-    zr1.add(circle);
+    ////////////////////////////////////////////
 
-    var secondCanvas = new WeCanvas("secondCanvas");
-    var zr2 = zrender.init(secondCanvas);
-    zr2.add(text);
+    var smoothPolygon = new PolygonShape({
+      style: {
+        // smoothConstraint: [[-Infinity, -Infinity], [200, Infinity]],
+        fill: 'rgba(220, 20, 60, 0.4)',
+        stroke: "rgba(220, 20, 60, 1)",
+        lineWidth: 2
+      },
+
+      shape: {
+        points: [[50, 50], [130, 20], [100, 140], [50, 150], [10, 70]],
+        smooth: 0.5
+      }
+    });
+    var ctx4 = new WeCanvas("canvas-4");
+    var zr4 = zrender.init(ctx4);
+    zr4.add(smoothPolygon);
   }
 })
